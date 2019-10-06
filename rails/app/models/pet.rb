1510adab70a6c9027ce9da5e4aa413e7b105b7b4
Jun Shimamura <jsmmr@icloud.com>
2019-10-07 00:05:47 +0900

    Add model annotations

--- rails/app/models/pet.rb
+++ rails/app/models/pet.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: pets
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  creature_id :integer          not null
+#  name        :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Pet < ApplicationRecord
   belongs_to :creature
   belongs_to :user
--- rails/app/models/question.rb
+++ rails/app/models/question.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  text       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Question < ApplicationRecord
   has_many :answers
 end

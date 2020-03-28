c531659dd9f404415620e365a70f26463fb31d0b
Jun Shimamura <jsmmr@icloud.com>
2020-03-28 17:20:25 +0900

    Run bundle exec annotate --models

--- rails/spec/factories/friends.rb
+++ rails/spec/factories/friends.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: friends
+#
+#  id             :bigint           not null, primary key
+#  user_id        :bigint           not null
+#  friend_user_id :bigint           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 FactoryBot.define do
   factory :friend do
     
--- rails/spec/factories/pets.rb
+++ rails/spec/factories/pets.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: pets
+#
+#  id          :bigint           not null, primary key
+#  user_id     :bigint           not null
+#  creature_id :bigint           not null
+#  name        :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 FactoryBot.define do
   factory :pet do
     user
